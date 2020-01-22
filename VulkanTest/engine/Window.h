5a2c9ee885c1cddb7ec8bd6b7b073b2f17512a17
Dejan Trajkovic <dtrajko@gmail.com>
2020-01-22 01:20:56 +0100

    GLFW event callbacks in Window class

--- VulkanTest/engine/Window.h
+++ VulkanTest/engine/Window.h
@@ -25,6 +25,15 @@ public:
 
 	static void framebufferResizeCallback(GLFWwindow* window, int width, int height);
 
+	void OnWindowResize(int width, int height);
+	void OnWindowClose();
+	void OnKeyPressed(int key, int scancode, int action, int mods, int repeatCount);
+	void OnKeyReleased(int key, int scancode, int action, int mods);
+	void OnMouseButtonPressed(int button, int action, int modes);
+	void OnMouseButtonReleased(int button, int action, int modes);
+	void OnMouseScrolled(float xOffset, float yOffset);
+	void OnMouseMoved(float xPos, float yPos);
+
 	~Window();
 
 };
